1f17a748a13bd3a67128997bbc2978d7121ba8c6
Majid Asadpoor <majid@centos82.rayka-co.local>
2021-03-13 23:48:18 +0330

    add bash folder

---
+++ bash/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set term type if needed (vt100 for Nexus)
+#export TERM=vt00
+
+# Rotate logs
+timestamp=`date +%Y%m%d%H%M`
+ 
+# Archive last config log
+logfile=config.log
+newlogfile=$logfile.$timestamp
+cp $logfile $newlogfile
+
+# Get SSH and enable passwords
+ echo -n "Enter your SSH username "
+ read -s -e username
+ echo -n "Enter your SSH password "
+ read -s -e password
+ echo -ne '\n'
+ echo -n "Enter your enable password "
+ read -s -e enable
+ echo -ne '\n'
+
+# Pull in device list and passwords
+for device in `cat devices.txt`; do
+ ./config.exp $device $username $password $enable ;
+ done
