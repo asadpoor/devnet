56deea4a832a55f5624fe413da7f4aa5aa14b55a
Majid Asadpoor <majid.asadpoor@gmail.com>
2022-06-11 23:47:18 +0200

    napalm

---
+++ pyhton_nornir/6.nornir_napalm_get_commands.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def napalm_get_example(task):
+    task.run(task=napalm_get, getters=["get_facts", "get_environment", "get_interfaces"])
+
+results=nr.run(task=napalm_get_example)
+print_result(results)
