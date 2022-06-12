8dd986abc4c6e19098529d79c87adf6076905ac9
Majid Asadpoor <majid.asadpoor@gmail.com>
2022-06-12 21:52:30 +0200

    napalm_cli

---
+++ pyhton_nornir/6.1.nornir_napalm_cli_commands.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_cli
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def napalm_cli_commands_example(task):
+    task.run(task=napalm_cli, commands=["show ip interface brief", "show version | inc XE"])
+
+results=nr.run(task=napalm_cli_commands_example)
+print_result(results)
