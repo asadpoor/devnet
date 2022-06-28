2126238b2c7c94ab0ef1034a4dde0ac93a009699
Majid Asadpoor <majid.asadpoor@gmail.com>
2022-06-29 00:16:17 +0200

    template part1

--- pyhton_nornir/11.nornir_jinja2_template.py
+++ pyhton_nornir/11.nornir_jinja2_template.py
@@ -1,3 +1,4 @@
+import logging
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_configs
 from nornir_utils.plugins.functions import print_result
@@ -9,11 +10,11 @@ nr = InitNornir(config_file="config.yaml")
 def load_data(task):
   hosts_data = task.run(task=load_yaml, file=f"./host_vars/{task.host}.yaml")
   groups_data = task.run(task=load_yaml, file=f"./group_vars/all.yaml")
-  task.host["data"] = hosts_data.result
+  task.host["hdata"] = hosts_data.result
   task.host["gdata"] = groups_data.result
 
 def template_snmp(task):
-    template = task.run(task=template_file, template=f"{task.host.platform}-ntp.j2", path="./")
+    template = task.run(task=template_file, template=f"{task.host.platform}-ntp.j2", path="./templates/", severity_level=logging.DEBUG)
     task.host["config"]=template.result
     configurations=task.host["config"].splitlines()
     task.run(task=send_configs, configs=configurations)
