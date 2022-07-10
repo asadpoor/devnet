4cc811b669870fa62afae759453597d125cc797d
Majid Asadpoor <majid.asadpoor@gmail.com>
2022-07-10 23:06:27 +0200

    12.1

--- pyhton_nornir/12.netbox/12.netbox_get_data.py
+++ pyhton_nornir/12.netbox/12.netbox_get_data.py
@@ -7,9 +7,9 @@ nr.inventory.defaults.username = "rayka"
 nr.inventory.defaults.password = "rayka-co.ir"
 
 def getnetboxdata(task):
-    rprint(f"My name is {task.host}")
-    rprint(f"My IP address is {task.host.hostname}")
-    rprint(f"My platform is {task.host.platform}")
-    rprint(f"My data is {task.host.data}")
+    rprint(f"device name: {task.host}")
+    rprint(f"device IP address: {task.host.hostname}")
+    rprint(f"device platform: {task.host.platform}")
+    rprint(f"device inventory data: {task.host.data}")
 
 nr.run(task=getnetboxdata)
