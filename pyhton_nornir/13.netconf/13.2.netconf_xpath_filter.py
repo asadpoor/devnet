bfb00758a06fd4d3850fcf49758713a8bcdc311d
asadpoor <majid.asadpoor@gmail.com>
2022-08-10 01:16:37 +0430

    netconf xpath filtering

--- pyhton_nornir/13.netconf/13.2.netconf_xpath_filter.py
+++ pyhton_nornir/13.netconf/13.2.netconf_xpath_filter.py
@@ -1,24 +1,27 @@
 from nornir import InitNornir
-from nornir_scrapli.tasks import netconf_get_config
-#from nornir_netconf.plugins.tasks import netconf_get_config
+#from nornir_scrapli.tasks import netconf_get_config
+from nornir_netconf.plugins.tasks import netconf_get_config
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
 
-def get_config(task):
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="/native")
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="/native/router")
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="/native/router/router-ospf")
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="//router-ospf")
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="/native//ospf")
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="//interfaces")
-#    task.run(task=netconf_get_config, filter_type="xpath", filter_="/native//interface")
-    task.run(task=netconf_get_config, filter_type="xpath", filter_="//bgp")
-
+def netconf_xpath(task):
+    task.run(task=netconf_get_config, source="running")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native/username")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native/ip/access-list/standard")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native/router")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native/router/router-ospf")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="//router-ospf")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native//ospf")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="//interfaces")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/native//interface")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="//bgp")
+#    task.run(task=netconf_get_config, source="running", filter_type="xpath", filter_="/network-instances//bgp")
 
 #    task.run(task=netconf_get_config, source="running", xmldict="false", filter_type="xpath", path="/native")
 #    task.run(task=netconf_get_config, source="running", xmldict="false", filter_type="xpath", path="/native/router/router-ospf")
 #    task.run(task=netconf_get_config, source="running", xmldict="false", filter_type="xpath", path="//router-ospf")
 
-results = nr.run(task=get_config)
+results = nr.run(task=netconf_xpath)
 print_result(results)
