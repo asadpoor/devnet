bdc049955f6cc4c755c57e26c34dcadb84d7a186
asadpoor <majid.asadpoor@gmail.com>
2022-08-13 15:32:45 +0430

    netconf xmltodict

---
+++ pyhton_nornir/13.netconf/13.6.nornir_scrapli_netconf_xmltodict.py
@@ -0,0 +1,35 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import netconf_get_config
+from nornir_utils.plugins.functions import print_result
+import xmltodict
+from pprint import pprint
+
+nr = InitNornir(config_file="config.yaml")
+
+filter1 = """
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+  </interfaces>
+"""
+
+
+def netconf_xmltodict(task):
+    interfaces = task.run(task=netconf_get_config, source="running", filter_type="subtree", filter_=filter1)
+    interfaces_dic = xmltodict.parse(interfaces.result)
+#    pprint(interfaces_dic)
+#    pprint(interfaces_dic["rpc-reply"])
+#    pprint(interfaces_dic["rpc-reply"]["data"])
+#    pprint(interfaces_dic["rpc-reply"]["data"]["interfaces"])
+#    pprint(interfaces_dic["rpc-reply"]["data"]["interfaces"]["interface"])
+#    pprint(type(interfaces_dic["rpc-reply"]["data"]["interfaces"]["interface"]))
+
+    interfaceslist = interfaces_dic["rpc-reply"]["data"]["interfaces"]["interface"]
+    for interface in interfaceslist:
+#        pprint(interface)
+        pprint(interface["name"])
+#        pprint(interface["ipv4"])
+#        pprint(interface["ipv4"]["address"])
+        pprint(interface["ipv4"]["address"]["ip"])
+
+
+
+results = nr.run(task=netconf_xmltodict)
---
+++ pyhton_nornir/13.netconf/13.7.nornir_netconf_xmltodict.py
@@ -0,0 +1,34 @@
+from nornir import InitNornir
+from nornir_netconf.plugins.tasks import netconf_get_config
+from nornir_utils.plugins.functions import print_result
+import xmltodict
+from pprint import pprint
+
+nr = InitNornir(config_file="config.yaml")
+
+filter1 = """
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+  </interfaces>
+"""
+
+
+def netconf_xmltodict(task):
+    interfaces = task.run(task=netconf_get_config, source="running", xmldict="true", filter_type="subtree", path=filter1)
+    interfaces_dic = interfaces.result
+#    pprint(interfaces_dic)
+#    pprint(interfaces_dic["xml_dict"])
+#    pprint(interfaces_dic["xml_dict"]["data"])
+
+    interfaceslist = interfaces_dic["xml_dict"]["data"]["interfaces"]["interface"]
+    pprint(type(interfaceslist))
+
+#    for key,value in interfaceslist.items():
+#        print("key=",key)
+#        print("value=",value)
+
+    for interface in interfaceslist:
+        pprint(interface["name"])
+        pprint(interface["ipv4"]["address"]["ip"])
+
+
+results = nr.run(task=netconf_xmltodict)
