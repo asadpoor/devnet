c6f45af854076eb88cfe0993eea8c36a88c33a9b
asadpoor <majid.asadpoor@gmail.com>
2022-12-25 14:37:23 +0330

    nornir napalmstructured output code example

--- pyhton_nornir/2023/5.nornir_napalm/5.3.nornir_napalm_get_result_parsing.py
+++ pyhton_nornir/2023/5.nornir_napalm/5.3.nornir_napalm_get_result_parsing.py
@@ -5,18 +5,21 @@ from rich import print as rprint
 
 nr = InitNornir(config_file="config.yaml")
 
-def nornir_napalm_get_example(task):
-    interfaces=task.run(task=napalm_get, getters=["get_interfaces"])
-#    config=task.run(task=napalm_get, getters=["get_config"])
+def nornir_napalm_structured_output_example(task):
+#    interfaces=task.run(task=napalm_get, getters=["get_interfaces"])
+    config=task.run(task=napalm_get, getters=["get_config"])
 
-#    print(interfaces.result)
-#    rprint(interfaces.result)
-#    rprint(interfaces.result["get_interfaces"])
-    rprint(interfaces.result["get_interfaces"]["GigabitEthernet1"])
-#    rprint(interfaces.result["get_interfaces"]["GigabitEthernet1"]["is_enabled"])
+### the result of the first example
+#    print(type(interfaces.result))   #1
+#    rprint(interfaces.result)        #2
+#    rprint(interfaces.result["get_interfaces"])    #3
+#    rprint(interfaces.result["get_interfaces"]["GigabitEthernet1"])     #4
+#    rprint(interfaces.result["get_interfaces"]["GigabitEthernet1"]["is_enabled"])   #5
 
-#    rprint(config.result)
-#    rprint(config.result["get_config"])
-#    rprint(config.result["get_config"]["running"])
 
-nr.run(task=nornir_napalm_get_example)
+### the result of the second example
+#    rprint(config.result)    #1
+    rprint(config.result["get_config"]) #2
+#    rprint(config.result["get_config"]["running"])    #3
+
+nr.run(task=nornir_napalm_structured_output_example)
