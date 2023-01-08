ac039df9abc47a2265b113f3ed1033e07bc014c6
asadpoor <majid.asadpoor@gmail.com>
2023-01-08 13:51:20 +0330

    get password by sysargv

--- pyhton_nornir/2023/6.avoid_clear_text_passwords_in_nornir_inventory/6.3.get_password_by_sysargv.py
+++ pyhton_nornir/2023/6.avoid_clear_text_passwords_in_nornir_inventory/6.3.get_password_by_sysargv.py
@@ -1,6 +1,6 @@
 import sys
 from nornir import InitNornir
-from nornir_netmiko.tasks import netmiko_send_command
+from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
@@ -8,9 +8,9 @@ nr = InitNornir(config_file="config.yaml")
 nr.inventory.hosts["R1"].username=sys.argv[1]
 nr.inventory.hosts["R1"].password=sys.argv[2]
 
-def netmiko_send_commands_example(task):
-    task.run(task=netmiko_send_command, command_string="show ip int brief | exc unass")
+def get_password_by_sysargv_example(task):
+    task.run(task=send_command, command="show ip interface brief | exc unassigned")
 
-results=nr.run(task=netmiko_send_commands_example)
+results=nr.run(task=get_password_by_sysargv_example)
 print_result(results)
 
