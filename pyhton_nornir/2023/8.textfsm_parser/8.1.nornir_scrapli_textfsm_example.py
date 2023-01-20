86ee332965d3686f63887d90a1f14d566ea484ce
Majid Asadpoor <majid.asadpoor@gmail.com>
2023-01-20 13:07:14 +0100

    textfsm parser

---
+++ pyhton_nornir/2023/8.textfsm_parser/8.1.nornir_scrapli_textfsm_example.py
@@ -0,0 +1,19 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from rich import print as rprint
+
+nr = InitNornir(config_file="config.yaml")
+
+def nornir_scrapli_textfsm_example(task):
+    result=task.run(task=send_command, command="show ip interface brief")
+    interfaces = result.scrapli_response.textfsm_parse_output()
+    rprint(interfaces)
+#    rprint(type(interfaces))
+
+#    for interface in interfaces:
+#        rprint(interface)
+#        rprint(type(interface))
+#        rprint("interface", interface["intf"], "with IP address", interface["ipaddr"] , "is physically ", interface["status"], "and line protocol is", interface["proto"])
+
+
+results=nr.run(task=nornir_scrapli_textfsm_example)
---
+++ pyhton_nornir/2023/8.textfsm_parser/8.2.nornir_netmiko_textfsm_example.py
@@ -0,0 +1,18 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from rich import print as rprint
+
+nr = InitNornir(config_file="config.yaml")
+
+def nornir_netmiko_textfsm_example(task):
+    result=task.run(task=netmiko_send_command, command_string="show interfaces", use_textfsm=True)
+    interfaces = result.result
+    rprint(interfaces)
+#    rprint(type(interfaces))
+
+#    for interface in interfaces:
+#        rprint(interface)
+#        rprint(type(interface))
+#        rprint("interface", interface["interface"], "with IP address", interface["ip_address"], "is physically ", interface["link_status"], "and line protocol is", interface["protocol_status"])
+
+results=nr.run(task=nornir_netmiko_textfsm_example)
