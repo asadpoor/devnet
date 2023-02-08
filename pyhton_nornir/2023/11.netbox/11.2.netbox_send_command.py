2364a99278391acf0e9f9f1057f2aa79eb648956
Majid Asadpoor <majid.asadpoor@gmail.com>
2023-02-08 22:16:34 +0100

    netbox_minimum_required_info

--- pyhton_nornir/2023/11.netbox/11.2.netbox_send_command.py
+++ pyhton_nornir/2023/11.netbox/11.2.netbox_send_command.py
@@ -6,15 +6,17 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 
+# not recommended to store password in python code
 #nr.inventory.defaults.username = "rayka"
 #nr.inventory.defaults.password = "rayka-co.com"
 
 # or read from environment variable already created in os
-#export NAPALM_USERNAME='rayka'
-#export NAPALM_PASSWORD='rayka-co.com'
+#majid@devnet:$ export NAPALM_USERNAME='rayka'
+#majid@devnet:$ export NAPALM_PASSWORD='rayka-co.com'
 nr.inventory.defaults.username = os.environ['NAPALM_USERNAME']
 nr.inventory.defaults.password = os.environ['NAPALM_PASSWORD']
 
+# or any other methods (like  piblic key authentication, sysargv, GPG or getpass, ...)
 
 def netbox_send_command(task):
     task.run(task=send_command, command="show ip interface brief")
