a441f145ffa3e7518737e69999dfbce9e184d4e7
asadpoor <majid.asadpoor@gmail.com>
2024-11-04 13:42:11 +0100

    pyats easypy

---
+++ pyats/8.1.easypy_testscript1.py
@@ -0,0 +1,29 @@
+from pyats import aetest
+from genie.testbed import load
+from pyats.async_ import pcall
+
+class Version_Check(aetest.Testcase):
+    @aetest.setup
+    def setup(self, testbed):
+        self.testbed = load(testbed)
+        self.device_list = [dev for dev in self.testbed.devices.values()]
+
+    @aetest.test
+    def check_version(self):
+        def get_version(device):
+            device.connect(log_stdout=False)
+
+            output = device.parse('show version')
+
+            print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            print("device=",device.name," ouput=",output['version']['version'])
+            print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+            device.disconnect()
+
+        # Use pcall correctly by passing the devices as a positional argument
+        pcall(get_version, device=self.device_list)
+
+
+if __name__ == '__main__':
+    aetest.main()
---
+++ pyats/8.2.easypy_testscript2.py
@@ -0,0 +1,26 @@
+import json
+from genie.testbed import load
+from pyats.async_ import pcall
+from pyats import aetest
+from genie.conf import Genie
+
+class PingCheck(aetest.Testcase):
+    @aetest.setup
+    def setup(self, testbed):
+        self.testbed = load(testbed)
+        self.device_list = [dev for dev in self.testbed.devices.values()]
+
+    @aetest.test
+    def check_ping(self):
+        def ping_test(device):
+            device.connect(log_stdout=False)
+            output = device.execute("ping 8.8.8.8")
+            print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            print("device=",device.name," ouput=",output)
+            print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            device.disconnect()
+
+        pcall(ping_test, device=self.device_list)
+
+
+aetest.main()
