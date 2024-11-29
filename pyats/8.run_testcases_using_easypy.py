e127ad260a032ab9197eab17920fb1334f1c0427
asadpoor <majid.asadpoor@gmail.com>
2024-11-29 12:08:30 +0100

    pyats easypy

---
+++ pyats/8.run_testcases_using_easypy.py
@@ -0,0 +1,10 @@
+from pyats.easypy import run
+
+def main():
+    run('8.1.easypy_testscript1.py')
+    run('8.2.easypy_testscript2.py')
+#    run('test.py')
+
+# pyats run job 8.run_testcases_using_easypy.py --testbed-file  testbed.yaml
+# pyats logs view
+
